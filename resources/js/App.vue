3e0bdcc3aaff2a9875b707f3b3591c78ac453c2f
KollarMaTT <kollarmatt@gmail.com>
2025-03-26 19:07:25 +0100

    Initial commit

---
+++ resources/js/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <main>
+      <TestPage/>
+    </main>
+  </div>
+</template>
+
+<script setup>
+  import TestPage from './Pages/TestPage.vue';
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/TestPage.vue
@@ -0,0 +1,12 @@
+<template>
+    <TestComponent/>
+</template>
+  
+<script setup>
+  import TestComponent from '../Components/TestComponent.vue';
+</script>
+  
+<style scoped>
+
+</style>
+  
